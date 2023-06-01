5aa444177fe9582808ab09cb44bf19c82affc8f2
John Doe <adamalfraih@gmail.com>
2023-06-01 18:46:12 +0200

    first commit, google oauth works

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,16 @@
 import { useLogin } from "@refinedev/core";
 import { useEffect, useRef } from "react";
-
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { ThemedTitleV2 } from "@refinedev/mui";
 
+import { yariga } from '../assets';
+
 import { CredentialResponse } from "../interfaces/google";
 
 // Todo: Update your Google Client ID here
-const GOOGLE_CLIENT_ID =
-  "1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com";
 
 export const Login: React.FC = () => {
   const { mutate: login } = useLogin<CredentialResponse>();
@@ -26,7 +26,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -68,8 +68,9 @@ export const Login: React.FC = () => {
             justifyContent: "center",
           }}
         />
-
-        <GoogleButton />
+        <Box mt={4}>
+          <GoogleButton />
+        </Box>
 
         <Typography align="center" color={"text.secondary"} fontSize="12px">
           Powered by
